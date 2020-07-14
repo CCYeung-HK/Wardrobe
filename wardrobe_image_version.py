30ee48fbe3afcc9628a4194dac9bdb2ae66beeb1
CCYeung-HK <ccy16@ic.ac.uk>
2020-07-14 04:04:14 +0100

    basically finish. Copy before start deleting unneccessary comments/codes

--- wardrobe_image_version.py
+++ wardrobe_image_version.py
@@ -57,10 +57,10 @@ class wardrobeApp:
     def create_buttons(self):
         #creating prev/next buttons
         #(master widget (frames), command = function to be called)
-        top_prev_button = tk.Button(self.tops_frame, text='Prev', command = self.get_prev_top)
+        top_prev_button = tk.Button(self.tops_frame, text='Give me some ideas', command = self.create_outfit)
         top_prev_button.pack(side=tk.LEFT)
 
-        top_next_button = tk.Button(self.tops_frame, text='Love it', command=self.get_next_top)
+        top_next_button = tk.Button(self.tops_frame, text='Love it', command=self.get_similar_outfit)
         top_next_button.pack(side=tk.RIGHT)
 
         # bottom_prev_button = tk.Button(self.bottom_frame, text='Prev', command = self.get_prev_bottom)
@@ -75,36 +75,36 @@ class wardrobeApp:
 
 
     #general fcn that will allow us to move front and back
-    def _get_next_item(self, current_item, category, increment = True):
-        #if we know where the current item index is in a category, then we find the pic before and after it
-        item_index = category.index(current_item)
-        final_index = len(category) - 1
-        next_index = 0
+    # def _get_next_item(self, current_item, category, increment = True):
+    #     #if we know where the current item index is in a category, then we find the pic before and after it
+    #     item_index = category.index(current_item)
+    #     final_index = len(category) - 1
+    #     next_index = 0
 
-        if increment and item_index == final_index:
-            #add the end, and need to up, cycle back to beginning
-            next_index = 0
-        elif not increment and item_index == 0:
-            #cycle back to end
-            next_index = final_index
-        else:
-            #regular up and down
-            #based on increment
-            increment = 1 if increment else -1
-            next_index = item_index + increment
+    #     if increment and item_index == final_index:
+    #         #add the end, and need to up, cycle back to beginning
+    #         next_index = 0
+    #     elif not increment and item_index == 0:
+    #         #cycle back to end
+    #         next_index = final_index
+    #     else:
+    #         #regular up and down
+    #         #based on increment
+    #         increment = 1 if increment else -1
+    #         next_index = item_index + increment
         
-        next_image = category[next_index]
+    #     next_image = category[next_index]
 
-        # reset update the image based on next_image path
-        if current_item in self.top_images:
-            image_label = self.top_image_label
-            self.top_image_path = next_image
-        else:
-            image_label = self.bottom_image_label
-            self.bottom_image_path = next_image
+    #     # reset update the image based on next_image path
+    #     if current_item in self.top_images:
+    #         image_label = self.top_image_label
+    #         self.top_image_path = next_image
+    #     else:
+    #         image_label = self.bottom_image_label
+    #         self.bottom_image_path = next_image
 
-        #use update function to change image
-        self.update_image(next_image, image_label)
+    #     #use update function to change image
+    #     self.update_image(next_image, image_label)
 
     def update_image(self, new_image_path, image_label):
         #collect and change image into tk photo obj
@@ -117,19 +117,19 @@ class wardrobeApp:
 
         image_label.image = tk_photo
 
-    def get_next_top(self): 
+    def get_similar_outfit(self): 
         #Now treat it as a like button
         # self._get_next_item(self.top_image_path, self.top_images)
         self.like_outfit(self.top_image_path, self.bottom_image_path)
 
-    def get_prev_top(self):
-        self._get_next_item(self.top_image_path, self.top_images, increment=False)
+    # def get_prev_top(self):
+    #     self._get_next_item(self.top_image_path, self.top_images, increment=False)
 
-    def get_next_bottom(self): 
-        self._get_next_item(self.bottom_image_path, self.bottom_images)
+    # def get_next_bottom(self): 
+    #     self._get_next_item(self.bottom_image_path, self.bottom_images)
 
-    def get_prev_bottom(self):
-        self._get_next_item(self.bottom_image_path, self.bottom_images, increment=False)
+    # def get_prev_bottom(self):
+        # self._get_next_item(self.bottom_image_path, self.bottom_images, increment=False)
 
     def create_photo(self, image_path, frame):
         #get PIL image
@@ -171,11 +171,6 @@ class wardrobeApp:
         self.bottom_image_path = reco_image_bottom
         self.update_image(reco_image_bottom, self.bottom_image_label)
 
-
-
-
-
-        
 
 root = tk.Tk()
 app = wardrobeApp(root)
